b190c3e8a7df9892710d341be01dbdf767d6ad13
hatimcontractor <148803018+hatimcontractor@users.noreply.github.com>
2023-11-14 13:18:25 +0530

    Update tradingbot.py

--- tradingbot.py
+++ tradingbot.py
@@ -3,59 +3,60 @@ import yfinance as yf
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from datetime import datetime
 
-def trading_strategy_sarimax(symbol='BTC-USD', short_period=12, long_period=26, rsi_period=14):
-    recommendation = None  # Initialize the recommendation variable
-
+def crypto_recommendation(symbol='BTC-USD', interval='1d', short_period=12, long_period=26, signal_period=9):
+    # Function to fetch historical data from Yahoo Finance
     def fetch_historical_data(symbol, start_date, end_date, interval):
         data = yf.download(symbol, start=start_date, end=end_date, interval=interval)
+        data.index.freq = 'D'  # Set the frequency explicitly
         return data
 
-    def calculate_macd(data, short_period, long_period):
+    # Function to calculate MACD and MACD histogram
+    def calculate_macd(data, short_period, long_period, signal_period):
         data['ema_short'] = data['Close'].ewm(span=short_period, adjust=False).mean()
         data['ema_long'] = data['Close'].ewm(span=long_period, adjust=False).mean()
         data['macd'] = data['ema_short'] - data['ema_long']
-        data['signal'] = data['macd'].ewm(span=9, adjust=False).mean()
+        data['signal'] = data['macd'].ewm(span=signal_period, adjust=False).mean()
+        data['macd_histogram'] = data['macd'] - data['signal']
 
-    def calculate_rsi(data, rsi_period):
-        delta = data['Close'].diff(1)
-        gain = delta.where(delta > 0, 0)
-        loss = -delta.where(delta < 0, 0)
-        avg_gain = gain.rolling(window=rsi_period).mean()
-        avg_loss = loss.rolling(window=rsi_period).mean()
-        rs = avg_gain / avg_loss
-        data['rsi'] = 100 - (100 / (1 + rs))
-
-    def load_and_predict_sarimax(data, start_date, end_date):
-        model = SARIMAX(data['Close'], order=(1, 1, 3), seasonal_order=(1, 1, 1, 12))
-        model_fit = model.fit(disp=False)
-        prediction = model_fit.get_forecast(start=start_date, end=end_date)
+    # Function to load and predict with SARIMAX model
+    def load_and_predict_sarimax(data, exog, start_date, end_date):
+        model = SARIMAX(data['Close'], exog=exog, order=(1, 1, 3), seasonal_order=(1, 1, 1, 12), freq='D')
+        
+        # Increase the number of iterations and try a different optimization algorithm
+        model_fit = model.fit(disp=False, maxiter=1000, method='powell')
+        
+        # Use the mean of exog for forecasting
+        exog_mean = exog.mean()
+        prediction = model_fit.get_forecast(steps=len(pd.date_range(start=start_date, end=end_date)), exog=[exog_mean])
         return prediction.predicted_mean
 
-    # Define symbol, start date, and end date
+    # Function for the trading strategy
+    def trading_strategy(data, exog, short_period, long_period, signal_period):
+        current_date = data.index[-1]
+        current_data = data.iloc[-1]
+
+        calculate_macd(data, short_period, long_period, signal_period)
+
+        # Use MACD histogram as an exogenous variable
+        exog = data['macd_histogram'].values.reshape(-1, 1)
+
+        sarimax_prediction = load_and_predict_sarimax(data, exog, current_date, current_date)
+
+        if sarimax_prediction.iloc[0] > current_data['Close']:
+            return 'Buy'
+        elif sarimax_prediction.iloc[0] < current_data['Close']:
+            return 'Sell'
+        else:
+            return 'Hold'
+
+    # Define start and end dates
     end_date = datetime.now().strftime('%Y-%m-%d')
-    start_date = (datetime.now() - pd.DateOffset(days=30)).strftime('%Y-%m-%d')
+    start_date = (datetime.now() - pd.DateOffset(days=365)).strftime('%Y-%m-%d')  # One year of historical data
 
     # Fetch historical data
-    historical_data = fetch_historical_data(symbol, start_date, end_date, '1d')
+    historical_data = fetch_historical_data(symbol, start_date, end_date, interval)
 
-    # Calculate MACD and RSI
-    calculate_macd(historical_data, short_period, long_period)
-    calculate_rsi(historical_data, rsi_period)
+    # Apply trading strategy with SARIMAX and MACD histogram as exogenous variable
+    signal = trading_strategy(historical_data, exog=None, short_period=short_period, long_period=long_period, signal_period=signal_period)
 
-    # Apply trading strategy with custom RSI and MACD functions
-    current_date = historical_data.index[-1]
-    current_data = historical_data.iloc[-1]
-
-    if current_data['macd'] > 0 and current_data['rsi'] < 70:
-        sarimax_prediction = load_and_predict_sarimax(historical_data, current_date, current_date)
-        if sarimax_prediction > current_data['Close']:
-            recommendation = 'Buy'
-    elif current_data['macd'] < 0 and current_data['rsi'] > 30:
-        sarimax_prediction = load_and_predict_sarimax(historical_data, current_date, current_date)
-        if sarimax_prediction < current_data['Close']:
-            recommendation = 'Sell'
-    else:
-        recommendation = 'Hold'
-
-    return recommendation
-
+    return signal
